76ff8c931b74d2e16d767228aa24271bef8dba95
DeltaTerran <96633825+DeltaTerran@users.noreply.github.com>
2025-01-07 16:18:40 +0200

    Украинская локализация

--- TCP-чат2/Tcp-чат2/Server/obj/Debug/net5.0/Server.AssemblyInfo.cs
+++ TCP-чат2/Tcp-чат2/Server/obj/Debug/net5.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa6de876bddcee4818ed900813178de62cc18ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb5066b4a41ae472d58e4572faa2c7296dbc369")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
